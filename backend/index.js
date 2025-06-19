68e197395f164858f0db55721827e48d157b34ce
Dennis van Leeuwen <bendora@gmail.com>
2025-06-19 11:12:45 +0200

    backend: index.js update

--- backend/index.js
+++ backend/index.js
@@ -16,33 +16,23 @@ const corsOptions = {
   credentials: true,
 };
 
-
 const app = express();
-// Initialize the database connection
+
 app.use(cors(corsOptions));
-// Set up CORS to allow requests from specific origins
-// and allow credentials (cookies, authorization headers, etc.)
+
 const PORT = process.env.PORT ?? 3001;
 const isProduction = process.env.ENVIRONMENT === 'production';
 
-// Middleware to parse JSON requests
 app.use(express.json());
 
-// Middleware to log HTTP requests in development mode
-// and set up Swagger documentation if not in production
 if (!isProduction) {
   app.use(morgan('dev'));
   setupSwagger(app, PORT);
 }
 
-// Define the routes for the application
-// Import the router from the routes directory
-// This router will handle all API endpoints
 app.use('/api', router);
 app.use(errorHandler);
 
-// Error handling middleware to catch any errors that occur in the application
-// This middleware will log the error and send a response to the client
 app.listen(PORT, () => {
   console.log('\x1b[33m%s\x1b[0m', `Server running on \x1b[36m ${corsOptions.origin[0]}:${PORT}`);
   if (!isProduction)
